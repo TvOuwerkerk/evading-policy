93f91aeb9dc80c387f37ef2065e0a22508cf88bc
Thomas van Ouwerkerk <t.b.vanouwerkerk@gmail.com>
2021-09-24 11:01:27 +0200

    Split text retrieval and tagging methods off. Made parallelisation possible. Set up sample tagging crawl.

--- Listing/Categorisation/LanguageTagging.py
+++ Listing/Categorisation/LanguageTagging.py
@@ -1,70 +1,100 @@
 import time
 from csv import reader
 from csv import writer
+from multiprocessing import Pool, TimeoutError
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.firefox import GeckoDriverManager
 from polyglot.detect import Detector
 from polyglot.detect.base import UnknownLanguage
-from tqdm import tqdm
+import random
 
 driver_path = GeckoDriverManager().install()
-driver = webdriver.Firefox(executable_path=driver_path)
+options = webdriver.FirefoxOptions()
+options.headless = True
+driver = webdriver.Firefox(executable_path=driver_path, firefox_options=options)
 
 
 def get_lan_code(text):
-    code = ''
-    text_bits = [text]
     if text == '':
         return 'Empty'
-    x = 0
-    while code == '' and x < 5:
-        start_len_bits = len(text_bits)
-        for y in range(start_len_bits):
-            piece = text_bits[y]
-            try:
-                detector = Detector(text)
-                if detector.reliable:
-                    return detector.language.code
-            except UnknownLanguage:
-                x += 1
-                text_bits.append(piece[:len(piece)])
-                text_bits[text_bits.index(piece)] = piece[len(piece):]
-
-    # return language with highest confidence level, unless 'nl' or 'en' is present with high enough confidence
+    try:
+        detector = Detector(text)
+        if detector.language.confidence >= 0.9 and detector.reliable:
+            return detector.language.code
+        return 'Unreliable'
+    except UnknownLanguage:
+        return 'Unknown'
 
 
-with open('BigQuery-McAfee-Cat.csv', 'r') as read_obj, open('BigQuery-McAfee-Cat_Lan.csv', 'w') as tagged:
-    csv_reader = reader(read_obj)
-    csv_writer = writer(tagged)
-    for row in tqdm(csv_reader):
-        # Download webpage text
-        try:
-            driver.get(row[0])
-        except TimeoutException:
-            with open('Lan-tagging_errors.csv', 'a') as errors:
-                errors.write(row[0] + " Retrieval timeout\n")
-            continue
-        text = driver.execute_script("return (!!document.body && document.body.innerText)")
-        if text == '':
-            time.sleep(2)
-            text = driver.find_element_by_xpath('/html/body').text
-        if text == '':
-            with open('Lan-tagging_errors.csv', 'a') as errors:
-                errors.write(row[0] + " Empty text\n")
-            continue
-        # Use polyglot to get language tag
-        lan_code = get_lan_code(text)
-        if lan_code == 'Empty':
-            with open('Lan-tagging_errors.csv', 'a') as errors:
-                errors.write(row[0] + " Empty text\n")
-            continue
-        if lan_code in ['Unknown', 'un', '']:
-            with open('Lan-tagging_errors.csv', 'a') as errors:
-                errors.write(row[0] + " Unknown language\n")
-            continue
-        row = [row[0]] + [lan_code] + row[1:]
-        row[-1] = row[-1].strip()
-        csv_writer.writerow(row)
+def get_text(site, driver):
+    try:
+        driver.get(site)
+    except TimeoutException:
+        return -1, [site] + [' Retrieval timeout\n']
+
+    text = driver.execute_script('return (!!document.body && document.body.innerText)')
+    if text == '':
+        time.sleep(2)
+        text = driver.find_element_by_xpath('/html/body').text
+    else:
+        return 0, text
+    if text == '':
+        return -1, [site] + [' Empty text\n']
+
+
+def tag_row(row):
+    if row is None:
+        driver.close()
+        return
+    try:
+        return_code, value = get_text(row[0], driver)
+    except Exception as exc:  # Catch problems like CAPTCHA or broken websites TODO: proper exception handling
+        print(str(exc) + ' exception encountered in getting')
+        print(row[0])
+        print(exc.args)
+        return -1, [row[0]] + [str(exc)]
+    if return_code == -1:
+        return return_code, value
+    try:
+        lan_code = get_lan_code(value)
+    except Exception as exc: # Catch unexpected problems in detecting languages TODO: proper exception handling
+        print(str(exc) + ' exception encountered in detecting')
+        print(row[0])
+        print(exc.args)
+        return -1, [row[0]] + [str(exc)]
+    if lan_code == 'Empty':
+        return -1, [row[0]] + ['Empty text\n']
+    if lan_code in ['Unknown', 'un', '']:
+        return -1, [row[0]] + ['Unknown language\n']
+    if lan_code == 'Unreliable':
+        return -1, [row[0]] + ['Low confidence\n']
+    row = [row[0]] + [lan_code] + row[1:]
+    return 0, row
+
+
+if __name__ == '__main__':
+    nr_runners = 1
+    with open('BigQuery-McAfee-Cat.csv', 'r', newline='') as read_obj, open('BigQuery-McAfee-Cat_Lan.csv', 'w',
+            newline='') as tagged, open('Tagging-errors.csv', 'w', newline='') as errors:
+        inp_list = [x for x in list(reader(read_obj)) if x is not None]
+        csv_output_writer = writer(tagged)
+        csv_error_writer = writer(errors)
+        inp = random.sample(inp_list, 100)
+        inp.append(None)
+        with Pool(nr_runners) as pool:
+            output = pool.map(tag_row, inp)  # TODO: clean up/close drivers when closing runners.
+
+        for line in output:  # TODO: have output runner using shared queue (Note: threads may be more suitable)
+            if line is None:
+                continue
+            if line[0] == -1:
+                csv_error_writer.writerow(line[1])
+                continue
+            elif line[0] == 0:
+                csv_output_writer.writerow(line[1])
+                continue
+            else:
+                print('Invalid code found in output')
     driver.close()
