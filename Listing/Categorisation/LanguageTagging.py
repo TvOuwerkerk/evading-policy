00851a80a70d9fd969b89b1c05953c02cb0f66d9
Thomas van Ouwerkerk <t.b.vanouwerkerk@gmail.com>
2021-09-28 11:47:04 +0200

    Let workers write to output file themselves.

--- Listing/Categorisation/LanguageTagging.py
+++ Listing/Categorisation/LanguageTagging.py
@@ -1,7 +1,7 @@
 import time
 from csv import reader
 from csv import writer
-from multiprocessing import Pool, TimeoutError
+from multiprocessing import Pool, TimeoutError, Lock
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -26,65 +26,70 @@ def get_lan_code(text):
     except UnknownLanguage:
         return 'Unknown'
 
+
 def get_inner_text(driver):
     return driver.execute_script('return (!!document.body && document.body.innerText)')
 
-def get_text(site):
-    driver = webdriver.Firefox(executable_path=driver_path, firefox_options=options)
+
+def get_text(site, driver):
+    print('getting ' + site)
     try:
         driver.get(site)
     except TimeoutException:
-        driver.quit()
+        print('Timeout reached getting: ' + site)
         return -1, [site] + [' Retrieval timeout\n']
     time.sleep(5)
     text = get_inner_text(driver)
     if text == '':
+        print('Second attempt getting text for: ' + site)
         time.sleep(5)
         text = get_inner_text(driver)
     else:
-        driver.quit()
         return 0, text
     if text == '':
-        driver.quit()
         return -1, [site] + [' Empty text\n']
 
 
 def tag_row(row):
+    driver = webdriver.Firefox(executable_path=driver_path, firefox_options=options)
     try:
-        return_code, value = get_text(row[0])
+        return_code, value = get_text(row[0], driver)
+        driver.quit()
     except Exception as exc:  # Catch problems like CAPTCHA or broken websites TODO: proper exception handling
         print(str(exc) + ' exception encountered in getting')
         print(row[0])
-        print(exc.args)
+        driver.quit()
         return -1, [row[0]] + [str(exc)]
     if return_code == -1:
         return return_code, value
     try:
         lan_code = get_lan_code(value)
-    except Exception as exc: # Catch unexpected problems in detecting languages TODO: proper exception handling
+    except Exception as exc:  # Catch unexpected problems in detecting languages TODO: proper exception handling
         print(str(exc) + ' exception encountered in detecting')
         print(row[0])
         print(exc.args)
         return -1, [row[0]] + [str(exc)]
-    if lan_code == 'Empty':
+    if lan_code == 'Empty':  # TODO Replace following with error logging
         return -1, [row[0]] + ['Empty text\n']
     if lan_code in ['Unknown', 'un', '']:
         return -1, [row[0]] + ['Unknown language\n']
     if lan_code == 'Unreliable':
         return -1, [row[0]] + ['Low confidence\n']
+
     row = [row[0]] + [lan_code] + row[1:]
-    return 0, row
+    with Lock():
+        with open('CRUX-McAfee-Cat_Lan.csv', 'a', newline='') as tagged:
+            csv_writer = writer(tagged)
+            csv_writer.writerow(row)
 
 
 if __name__ == '__main__':
-    nr_runners = 1
-    with open('CRUX-McAfee-Cat.csv', 'r', newline='') as read_obj, open('CRUX-McAfee-Cat_Lan.csv', 'w',
-            newline='') as tagged, open('Tagging-errors.csv', 'w', newline='') as errors:
+    nr_runners = 4
+    with open('CRUX-McAfee-Cat.csv', 'r', newline='') as read_obj, \
+            open('Tagging-errors.csv', 'w', newline='') as errors:
         inp_list = [x for x in list(reader(read_obj)) if x is not None]
-        csv_output_writer = writer(tagged)
         csv_error_writer = writer(errors)
-        inp = random.sample(inp_list, 100)
-        inp.append(None)
+        inp = random.sample(inp_list, 10)
         with Pool(nr_runners) as pool:
             output = pool.map(tag_row, inp)
 
@@ -93,9 +98,6 @@ if __name__ == '__main__':
                 continue
             if line[0] == -1:
                 csv_error_writer.writerow(line[1])
-                continue
-            elif line[0] == 0:
-                csv_output_writer.writerow(line[1])
                 continue
             else:
                 print('Invalid code found in output')
