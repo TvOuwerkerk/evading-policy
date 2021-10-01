26a830e6b01c0de0e1c2fc68040005c7c777c203
Thomas van Ouwerkerk <t.b.vanouwerkerk@gmail.com>
2021-10-01 16:43:37 +0200

    Added logging.

--- Listing/Categorisation/LanguageTagging.py
+++ Listing/Categorisation/LanguageTagging.py
@@ -1,30 +1,33 @@
 import time
+import random
+import logging
 from csv import reader
 from csv import writer
-from multiprocessing import Pool, TimeoutError, Lock
+from multiprocessing import Pool, Lock
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.firefox import GeckoDriverManager
+
 from polyglot.detect import Detector
 from polyglot.detect.base import UnknownLanguage
-import random
 
 driver_path = GeckoDriverManager().install()
 options = webdriver.FirefoxOptions()
 options.headless = True
 
+timestamp = time.strftime('%d-%b-%Y_%H%M', time.localtime())
+logname = 'LanguageTagging_' + timestamp + '.log'
+logging.basicConfig(filename=logname, level=logging.INFO)
+
 
 def get_lan_code(text):
     if text == '':
         return 'Empty'
-    try:
-        detector = Detector(text)
-        if detector.language.confidence >= 0.9 and detector.reliable:
-            return detector.language.code
-        return 'Unreliable'
-    except UnknownLanguage:
-        return 'Unknown'
+    detector = Detector(text)
+    if detector.language.confidence >= 0.9 and detector.reliable:
+        return detector.language.code
+    return 'Unreliable'
 
 
 def get_inner_text(driver):
@@ -32,22 +35,19 @@ def get_inner_text(driver):
 
 
 def get_text(site, driver):
-    print('getting ' + site)
-    try:
-        driver.get(site)
-    except TimeoutException:
-        print('Timeout reached getting: ' + site)
-        return -1, [site] + [' Retrieval timeout\n']
+    logging.info(' - Retrieving - ' + site)
+    driver.get(site)
+    # TODO:  log actual site being retrieved after any possible redirections
     time.sleep(5)
     text = get_inner_text(driver)
     if text == '':
-        print('Second attempt getting text for: ' + site)
         time.sleep(5)
+        logging.info(' - Retrieving: Retry - ' + site)
         text = get_inner_text(driver)
-    else:
-        return 0, text
     if text == '':
-        return -1, [site] + [' Empty text\n']
+        logging.warning(' - Retrieving: Empty text - ' + site)
+        return None
+    return 0, text
 
 
 def tag_row(row):
@@ -55,26 +55,35 @@ def tag_row(row):
     try:
         return_code, value = get_text(row[0], driver)
         driver.quit()
-    except Exception as exc:  # Catch problems like CAPTCHA or broken websites TODO: proper exception handling
-        print(str(exc) + ' exception encountered in getting')
-        print(row[0])
+    except TimeoutException:
+        logging.exception(' - Retrieving: Timeout - ' + row[0])
+        return
+    except Exception as exc:  # Catch problems like CAPTCHA or broken websites
+        # TODO: Catch specific exceptions that can occur during retrieval.
+        logging.exception('- Retrieving - ' + row[0] + '\n' + str(exc))
         driver.quit()
-        return -1, [row[0]] + [str(exc)]
-    if return_code == -1:
-        return return_code, value
+        return
+    if return_code is None:
+        return
+
     try:
         lan_code = get_lan_code(value)
-    except Exception as exc:  # Catch unexpected problems in detecting languages TODO: proper exception handling
-        print(str(exc) + ' exception encountered in detecting')
-        print(row[0])
-        print(exc.args)
-        return -1, [row[0]] + [str(exc)]
-    if lan_code == 'Empty':  # TODO Replace following with error logging
-        return -1, [row[0]] + ['Empty text\n']
+    except UnknownLanguage as unknown:
+        lan_code = 'Unknown'
+    except Exception as exc:  # Catch unexpected problems in detecting languages
+        # TODO: Catch specific exceptions that can occur during tagging.
+        logging.exception('- Tagging - ' + row[0] + '\n' + str(exc))
+        driver.quit()
+        return
+    if lan_code == 'Empty':
+        logging.warning('Tagging: Empty text - ')
+        return
     if lan_code in ['Unknown', 'un', '']:
-        return -1, [row[0]] + ['Unknown language\n']
+        logging.warning('Tagging: Unknown language - ')
+        return
     if lan_code == 'Unreliable':
-        return -1, [row[0]] + ['Low confidence\n']
+        logging.warning('Tagging: Low confidence - ')
+        return
 
     row = [row[0]] + [lan_code] + row[1:]
     with Lock():
@@ -84,20 +93,11 @@ def tag_row(row):
 
 
 if __name__ == '__main__':
-    nr_runners = 4
+    nr_runners = 8
     with open('CRUX-McAfee-Cat.csv', 'r', newline='') as read_obj, \
             open('Tagging-errors.csv', 'w', newline='') as errors:
         inp_list = [x for x in list(reader(read_obj)) if x is not None]
         csv_error_writer = writer(errors)
-        inp = random.sample(inp_list, 10)
+        inp = random.sample(inp_list, 200)
         with Pool(nr_runners) as pool:
-            output = pool.map(tag_row, inp)
-
-        for line in output:  # TODO: have output runner using shared queue (Note: threads may be more suitable)
-            if line is None:
-                continue
-            if line[0] == -1:
-                csv_error_writer.writerow(line[1])
-                continue
-            else:
-                print('Invalid code found in output')
+            pool.map(tag_row, inp)
