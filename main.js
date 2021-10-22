fbe0c4a30fafd478c369ef3b53d4cd9d427644a2
Thomas van Ouwerkerk <t.b.vanouwerkerk@gmail.com>
2021-10-22 12:00:24 +0200

    Add ScreenshotCollector

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const APICallCollector = require('./collectors/APICallCollector');
 const CookieCollector = require('./collectors/CookieCollector');
 const TargetCollector = require('./collectors/TargetCollector');
 const TraceCollector = require('./collectors/TraceCollector');
-//TODO: add screenshotCollector
+const ScreenshotCollector = require('./collectors/ScreenshotCollector');
 
 // reexport main pieces of code so that they can be easily imported when this project is used as a dependency
 // e.g. `const {crawlerConductor} = require('3p-crawler');`
@@ -18,5 +18,6 @@ module.exports = {
     APICallCollector,
     CookieCollector,
     TargetCollector,
-    TraceCollector
+    TraceCollector,
+    ScreenshotCollector
 };
