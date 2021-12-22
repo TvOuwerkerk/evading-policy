1f2b38fdef06fb6fba4b12f55204781d62206bc7
Thomas van Ouwerkerk <t.b.vanouwerkerk@gmail.com>
2021-12-22 10:48:08 +0100

    Fix accidental deletion of function call

--- Analysis/deleteResults.py
+++ Analysis/deleteResults.py
@@ -3,13 +3,13 @@ import os
 import glob
 
 
-DATA_PATH = os.path.join('sampledata3')
+DATA_PATH = os.path.join('Corpus-Head-crawl')
 
 data_directories = [x for x in os.listdir(DATA_PATH) if x.startswith('data.')]
 for directory in data_directories:
     admin_directory = os.path.join(DATA_PATH, directory)
-    admin_file_path = os.path.join(admin_directory, 'admin.*.json')[0]
-    with open(admin_file_path, 'r+') as admin:
+    admin_file_path = glob.glob(os.path.join(admin_directory, 'admin.*.json'))[0]
+    with open(admin_file_path, 'r+', encoding='utf-8') as admin:
         admin_data = json.load(admin)
         try:
             admin_data['results'] = []
